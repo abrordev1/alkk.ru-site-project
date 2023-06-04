f03f0747cface0980602ad20c0e93850250b49e1
abrordev1 <abrortajimuratov833@gmail.com>
2023-06-04 23:31:23 +0500

    init: startkit is done

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <HeaderLayout />
+    <RouterView />
+    <FooterLayout />
+  </main>
+</template>
+
+<script>
+import HeaderLayout from './layouts/HeaderLayouts/HeaderLayout.vue';
+import FooterLayout from './layouts/FooterLayouts/FooterLayout.vue';
+
+export default {
+  components: {
+    HeaderLayout,
+    FooterLayout
+  }
+}
+</script>
+
