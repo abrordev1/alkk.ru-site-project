59f70a8793796f485588790b9d1e9403a8cc2902
abrordev1 <abrortajimuratov833@gmail.com>
2023-06-07 21:42:16 +0500

    feat: added new ui elements & added some tailwind configs (2023-06-07, 21:31)

--- src/ui/TheTitle.vue
+++ src/ui/TheTitle.vue
@@ -1,5 +1,5 @@
 <template> 
-    <h2 class="text-[45px] leading-[45px] font-bold text-dark lg:text-[55px]">
+    <h2 class="text-[45px] leading-[45px] font-['FiraSansExtraCondensed'] font-bold text-dark lg:text-[55px]">
         <slot></slot>
     </h2>
     <img src="../../src/assets/images/fire.svg" alt="fire">
